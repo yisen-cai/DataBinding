46464cb78c3c01d013cab656259f1a06eb0032fa
yisen614 <yisen614@gmail.com>
2018-11-13 15:36:03 +0800

    include

--- app/src/main/java/top/yisen614/databinding/DataBindingActivity.kt
+++ app/src/main/java/top/yisen614/databinding/DataBindingActivity.kt
@@ -3,6 +3,7 @@ package top.yisen614.databinding
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import top.yisen614.databinding.databinding.ActivityBindingTestBinding
 import top.yisen614.databinding.databinding.ActivityDataBindingBinding
 import top.yisen614.databinding.persistence.entity.User
 
@@ -11,6 +12,7 @@ class DataBindingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding: ActivityDataBindingBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
-        binding.user = User("yisen614", 10, 10)
+        val binding1: ActivityBindingTestBinding = DataBindingUtil.setContentView(this, R.layout.activity_binding_test)
+        binding1.user = User("yisen614", 10, 10)
     }
 }
